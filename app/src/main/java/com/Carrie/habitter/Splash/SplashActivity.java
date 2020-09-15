f2028e02cd52bc0fdde0e47d31410283504906af
carrie <carrie0129@naver.com>
2020-09-15 10:44:53 +0900

    add publishing register

---
+++ app/src/main/java/com/Carrie/habitter/Splash/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.Carrie.habitter.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.Carrie.habitter.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
