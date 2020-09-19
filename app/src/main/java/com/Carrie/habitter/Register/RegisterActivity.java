a8d78b61e765b72491b75b97b95dbb8285da6719
carrie <carrie0129@naver.com>
2020-09-19 23:05:46 +0900

    modify publishing

--- app/src/main/java/com/Carrie/habitter/Register/RegisterActivity.java
+++ app/src/main/java/com/Carrie/habitter/Register/RegisterActivity.java
@@ -3,14 +3,44 @@ package com.Carrie.habitter.Register;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.Carrie.habitter.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button how_use;
+    Button start_with_analysis;
+    Button start_not_analysis;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+
+        how_use.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        start_with_analysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        start_not_analysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/Carrie/habitter/Splash/SplashActivity.java
+++ app/src/main/java/com/Carrie/habitter/Splash/SplashActivity.java
@@ -2,9 +2,14 @@ package com.Carrie.habitter.Splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 import com.Carrie.habitter.R;
+import com.Carrie.habitter.Register.RegisterActivity;
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,5 +17,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // 만약 shared preference 에 로그인 정보가 있다면, 개인 페이지로 넘어가고
+        // 로그인 정보가 없다면, 회원가입 화면으로
+
+        Handler hand = new Handler();
+        hand.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
     }
 }
